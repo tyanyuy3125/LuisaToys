39080e05b7b7de7bc2699826babe3cb7f55b9ee0
Tianyu Huang <tianyu@illumiart.net>
2024-04-08 11:41:33 +0800

    First working demo.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 
-#include "luisa-compute.h"
+#include <luisa-compute.h>
+#include <lodepng.h>
 
 using namespace luisa;
 using namespace luisa::compute;
-
-#include <unistd.h>
-[[nodiscard]] auto get_current_exe_path() noexcept {
-    char pathbuf[PATH_MAX] = {};
-    for (auto p : {"/proc/self/exe", "/proc/curproc/file", "/proc/self/path/a.out"}) {
-        if (auto size = readlink(p, pathbuf, sizeof(pathbuf)); size > 0) {
-            luisa::string_view path{pathbuf, static_cast<size_t>(size)};
-            return std::filesystem::canonical(path).string();
-        }
-    }
-}
 
 int main(int, char** argv){
     Context context{argv[0]};
     Device device = context.create_device("cuda");
     Stream stream = device.create_stream();
+
+    Image<float> device_image = device.create_image<float>(PixelStorage::BYTE4, make_uint2(1024u, 1024u), 0u);
+    Kernel2D kernel = [](Var<Image<float>> image) noexcept {
+        Var coord = dispatch_id().xy();
+        image->write(coord, make_float4(1.0f, 0.0f, 0.0f, 1.0f));
+    };
+
+    auto fill_image = device.compile(kernel);
+    std::vector<unsigned char> downloaded_image(1024u * 1024u * 4u);
+    stream << fill_image(device_image.view(0)).dispatch(1024u, 1024u) << device_image.copy_to(downloaded_image.data()) << synchronize();
+
+    lodepng::State state;
+    std::vector<unsigned char> png;
+    unsigned error = lodepng::encode(png, downloaded_image, 1024u, 1024u, state);
+    if(!error) lodepng::save_file(png, "output.png");
+
+    //if there's an error, display it
+    if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
 }
